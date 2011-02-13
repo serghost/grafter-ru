5bab75387dcadbd1e9f5f40c6a1fb6126b5a0e5e
Timothy Klim <klimtimothy@gmail.com>
2011-02-13 12:01:24 +0300

    First init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Grafter::Application.config.secret_token = '28c9aca336c92393ca4bc99373a15809973e2dcf0046c8d5428b0948f09a3ca26cb74c936c9d12dde197c402cdacf1f59f7c15c65c64c2cf381dea1aed8e0889'
