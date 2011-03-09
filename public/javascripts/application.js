8d555fe6046ba2ded2ff8833e140785f8117ea1b
Timothy Klim <klimtimothy@gmail.com>
2011-03-09 11:03:19 +0300

    Devise views installed

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,17 +1,17 @@
 $(function() {
 
-  $(".section, ul.tabs li.current").noisy({
-    noise: 0.9,
-    titleSize: 200,
-    backgroundColor: '#fafafa',
-    maxNoiseOpacity: 0.055,
-    monochromatic: false
-  });
+  // $(".section, ul.tabs li.current").noisy({
+  //   noise: 0.9,
+  //   titleSize: 200,
+  //   backgroundColor: '#fafafa',
+  //   maxNoiseOpacity: 0.055,
+  //   monochromatic: false
+  // });
 
   $("#add_price").click(function(){
     $("#price_dialog").dialog({
-      height: 500,
-      width: 470,
+      // height: 400,
+      width: 480,
       resizable: false,
       zIndex: 10000,
       modal: true
