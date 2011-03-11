72c9529e9aa2c921a7a5e60cda09836314781caa
Timothy Klim <klimtimothy@gmail.com>
2011-03-11 15:58:39 +0300

    paperclip fix

--- app/models/university.rb
+++ app/models/university.rb
@@ -1,11 +1,11 @@
 class University < ActiveRecord::Base
   belongs_to :city
 
-  has_attached_file :logo, :styles => {:thumb => '32x32', :original => '64x64'},
-    :url => "/images/:class/:attachment/:id/:style_:basename.:extension",
-    :default_url => "/images/:class/missing.png"
-
   validates :short, :presence => true, :length => {:in => 2..10}
   validates :title, :uniqueness => true, :presence => true, :length => {:minimum => 7}
   validates :city_id, :presence => true
+
+  has_attached_file :logo, :styles => {:thumb => '32x32', :original => '64x64'},
+    :url => "/images/:class/:attachment/:id/:style.:extension",
+    :default_url => "/images/:class/missing.png"
 end
