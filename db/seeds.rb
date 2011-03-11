8f1253b5b990df2da6184a7feb7036109fb426eb
Timothy Klim <klimtimothy@gmail.com>
2011-03-11 14:37:32 +0300

    Department and city finished

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,9 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ :name => 'Chicago' }, { :name => 'Copenhagen' }])
-#   Mayor.create(:name => 'Daley', :city => cities.first)
+# Add cities to department from russia db
+db_path = File.expand_path("../russia.json", __FILE__)
+russia_db = open(db_path).read
+JSON::parse(russia_db).each do |item|
+  department = Department.create!(:title => item["department"])
+  item["cities"].each do |city|
+    department.cities.create!(:title => city)
+  end
+end
