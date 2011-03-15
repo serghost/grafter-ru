5abccc4001faef0dcacae020293864685f77e9cf
Timothy Klim <klimtimothy@gmail.com>
2011-03-15 20:24:42 +0300

    Add noise

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,12 +1,12 @@
 $(function() {
 
-  // $(".section, ul.tabs li.current").noisy({
-  //   noise: 0.9,
-  //   titleSize: 200,
-  //   backgroundColor: '#fafafa',
-  //   maxNoiseOpacity: 0.055,
-  //   monochromatic: false
-  // });
+  $("#section, ul#tabs li.current").noisy({
+    noise: 0.9,
+    titleSize: 200,
+    backgroundColor: '#fafafa',
+    maxNoiseOpacity: 0.055,
+    monochromatic: false
+  });
 
   $("#add_price").click(function(){
     $("#price_dialog").dialog({
